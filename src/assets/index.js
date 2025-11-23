acc65c6dba8e195aa6a24d7a6326379d4f42b8d7
Samantha Anupriya <samanthaanupriya98@gmail.com>
2025-11-23 19:06:08 +0800

    saving new version

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import search from './search.svg';
+import menu from './menu.svg';
+import close from './close.svg';
+import calendar from './calendar.svg';
+
+export const assets = {
+    logo,
+    search,
+    menu,
+    close,
+    calendar,
+};
+
+export const cities = [
+    "Dubai",
+    "Singapore",
+    "New York",
+    "London",
+];
